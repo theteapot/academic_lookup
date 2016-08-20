185db417b653f5de9a59cb72094a0f5407b1d51c
Taylor kettle <tkettle@localhost.localdomain>
2016-08-20 14:33:30 +1200

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,31 @@
+$("#success").click(function () {
+    $(function () {
+        var params = {
+            // Request parameters
+            "query": $("#query-input").val(),
+            "model": "latest",
+            "count": "10",
+            "offset": "0",
+            "timeout": "2000"
+        };
+        alert($("#query-input").val());
+        $.ajax({
+            url: "https://api.projectoxford.ai/academic/v1.0/interpret?" + $.param(params),
+            beforeSend: function (xhrObj) {
+                // Request headers
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "6958f760d7074d1692f1262fa1052c7d");
+            },
+            type: "GET",
+            // Request body
+            data: "{body}"
+        })
+            .done(function (data) {
+            alert("success");
+            response = JSON.parse(data);
+            alert(response.query);
+        })
+            .fail(function () {
+            alert("error");
+        });
+    });
+});
